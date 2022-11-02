f10a556ffa8a351cc0729375ce2c428ef64eb491
kyon <kyon.0308@gmail.com>
2022-11-02 16:48:54 +0900

    upadte

---
+++ python3.9-image/cookiecutter-aws-sam-hello-python-lambda-image/{{cookiecutter.project_name}}/src/main.py
@@ -0,0 +1,12 @@
+import json
+
+
+def main(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+            }
+        ),
+    }
